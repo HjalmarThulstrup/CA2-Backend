4553f59732972f8de61f338e3ea88f557f2a532c
Martin <martinmoellerzbc@gmail.com>
2018-10-03 11:37:19 +0200

    added exceptions

---
+++ src/main/java/exceptions/CityNotFoundExceptionMapper.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import javax.servlet.ServletContext;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+/**
+ *
+ * @author martin
+ */
+public class CityNotFoundExceptionMapper implements ExceptionMapper<CityNotFoundException> {
+
+	@Context
+	ServletContext context;
+	static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+	@Override
+	public Response toResponse(CityNotFoundException e) {
+		boolean isDebug = context.getInitParameter("debug").equals("true");
+		ExceptionDTO err = new ExceptionDTO(e, 404, isDebug);
+		err.setDescription("You tried to call...");
+
+		return Response.status(404)
+				.entity(gson.toJson(err))
+				.type(MediaType.APPLICATION_JSON).
+				build();
+
+	}
+
+}

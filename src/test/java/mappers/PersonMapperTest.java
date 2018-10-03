49db46515f61d5151f12c97d89f78a8f153d5c1c
Martin <martinmoellerzbc@gmail.com>
2018-10-03 14:40:31 +0200

    added unfinished personMapper test

---
+++ src/test/java/mappers/PersonMapperTest.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mappers;
+
+import entity.CityInfo;
+import entity.Hobby;
+import entity.Person;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author martin
+ */
+public class PersonMapperTest {
+	
+	public PersonMapperTest() {
+	}
+	/**
+	 * Test of getByPhone method, of class PersonMapper.
+	 */
+//	@Test
+//	public void testGetByPhone() {
+//		System.out.println("getByPhone");
+//		int phone = 0;
+//		Person expResult = null;
+//		Person result = PersonMapper.getByPhone(phone);
+//		assertEquals(expResult, result);
+//		// TODO review the generated test code and remove the default call to fail.
+//		fail("The test case is a prototype.");
+//	}
+//
+//	/**
+//	 * Test of deletePersonById method, of class PersonMapper.
+//	 */
+//	@Test
+//	public void testDeletePersonById() {
+//		System.out.println("deletePersonById");
+//		int id = 0;
+//		PersonMapper.deletePersonById(id);
+//		// TODO review the generated test code and remove the default call to fail.
+//		fail("The test case is a prototype.");
+//	}
+//
+//	/**
+//	 * Test of editPerson method, of class PersonMapper.
+//	 */
+//	@Test
+//	public void testEditPerson() {
+//		System.out.println("editPerson");
+//		Person person = null;
+//		Person expResult = null;
+//		Person result = PersonMapper.editPerson(person);
+//		assertEquals(expResult, result);
+//		// TODO review the generated test code and remove the default call to fail.
+//		fail("The test case is a prototype.");
+//	}
+//
+//	/**
+//	 * Test of getPeopleByHobby method, of class PersonMapper.
+//	 */
+//	@Test
+//	public void testGetPeopleByHobby() {
+//		System.out.println("getPeopleByHobby");
+//		Hobby hobby = null;
+//		List<Person> expResult = null;
+//		List<Person> result = PersonMapper.getPeopleByHobby(hobby);
+//		assertEquals(expResult, result);
+//		// TODO review the generated test code and remove the default call to fail.
+//		fail("The test case is a prototype.");
+//	}
+//
+//	/**
+//	 * Test of getPeopleByCity method, of class PersonMapper.
+//	 */
+//	@Test
+//	public void testGetPeopleByCity() {
+//		System.out.println("getPeopleByCity");
+//		CityInfo city = null;
+//		List<Person> expResult = null;
+//		List<Person> result = PersonMapper.getPeopleByCity(city);
+//		assertEquals(expResult, result);
+//		// TODO review the generated test code and remove the default call to fail.
+//		fail("The test case is a prototype.");
+//	}
+//
+//	/**
+//	 * Test of createPerson method, of class PersonMapper.
+//	 */
+//	@Test
+//	public void testCreatePerson() {
+//		System.out.println("createPerson");
+//		Person person = null;
+//		Person expResult = null;
+//		Person result = PersonMapper.createPerson(person);
+//		assertEquals(expResult, result);
+//		// TODO review the generated test code and remove the default call to fail.
+//		fail("The test case is a prototype.");
+//	}
+//	
+}

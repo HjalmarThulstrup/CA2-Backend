a3f7d0b1ca8d52fb33360b4b860ea33d9c44d25d
RasmusLumholdt <dev.rlumholdt@gmail.com>
2018-10-03 11:21:54 +0200

    Added second pu, but fails

---
+++ src/test/java/mappers/HobbyMapperTest.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mappers;
+
+import DTO.HobbyDTO;
+import entity.Hobby;
+import javax.persistence.EntityManager;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author rlumh
+ */
+public class HobbyMapperTest {
+    
+    public HobbyMapperTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+
+//    @Test
+//    public void testGetEntityManager() {
+//        System.out.println("getEntityManager");
+//        HobbyMapper instance = null;
+//        EntityManager expResult = null;
+//        EntityManager result = instance.getEntityManager();
+//        assertEquals(expResult, result);
+//        fail("The test case is a prototype.");
+//    }
+//
+//    @Test
+//    public void testAddHobby() {
+//        System.out.println("addHobby");
+//        Hobby hobby = null;
+//        HobbyMapper instance = null;
+//        HobbyDTO expResult = null;
+//        HobbyDTO result = instance.addHobby(hobby);
+//        assertEquals(expResult, result);
+//        fail("The test case is a prototype.");
+//    }
+//
+//    @Test
+//    public void testDeleteHobby() {
+//        System.out.println("deleteHobby");
+//        int id = 0;
+//        HobbyMapper instance = null;
+//        HobbyDTO expResult = null;
+//        HobbyDTO result = instance.deleteHobby(id);
+//        assertEquals(expResult, result);
+//        fail("The test case is a prototype.");
+//    }
+//
+//    @Test
+//    public void testEditHobby() {
+//        System.out.println("editHobby");
+//        Hobby hobby = null;
+//        HobbyMapper instance = null;
+//        HobbyDTO expResult = null;
+//        HobbyDTO result = instance.editHobby(hobby);
+//        assertEquals(expResult, result);
+//        fail("The test case is a prototype.");
+//    }
+//
+//    @Test
+//    public void testGetHobbyPopularity() {
+//        System.out.println("getHobbyPopularity");
+//        Hobby hobby = null;
+//        HobbyMapper instance = null;
+//        int expResult = 0;
+//        int result = instance.getHobbyPopularity(hobby);
+//        assertEquals(expResult, result);
+//        fail("The test case is a prototype.");
+//    }
+//
+//    @Test
+//    public void testGetHobby() {
+//        System.out.println("getHobby");
+//        int id = 0;
+//        HobbyMapper instance = null;
+//        HobbyDTO expResult = null;
+//        HobbyDTO result = instance.getHobby(id);
+//        assertEquals(expResult, result);
+//        fail("The test case is a prototype.");
+//    }
+    
+}

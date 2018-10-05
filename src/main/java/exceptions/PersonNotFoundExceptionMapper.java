141421671f1977a59dd18fbd67f7af399a1f6a77
Martin <martinmoellerzbc@gmail.com>
2018-10-05 10:58:40 +0200

    added personNotFoundExceptionMapper

---
+++ src/main/java/exceptions/PersonNotFoundExceptionMapper.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import javax.servlet.ServletContext;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+/**
+ *
+ * @author martin
+ */
+public class PersonNotFoundExceptionMapper implements ExceptionMapper<PersonNotFoundException> {
+
+	@Context
+	ServletContext context;
+	static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+	@Override
+	public Response toResponse(PersonNotFoundException e) {
+		boolean isDebug = context.getInitParameter("debug").equals("true");
+		ExceptionDTO err = new ExceptionDTO(e, 404, isDebug);
+
+		return Response.status(404)
+				.header("Access-Control-Allow-Origin", "*")
+				.entity(gson.toJson(err))
+				.type(MediaType.APPLICATION_JSON).
+				build();
+
+	}
+
+}
